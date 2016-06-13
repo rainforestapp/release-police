517566dfab7abb41222d3749fa543bd767a14eb1
Nicolas Valcarcel <nvalcarcel@gmail.com>
2016-06-13 12:00:30 -0500

    resolve PD incidents.

--- main.rb
+++ main.rb
@@ -23,7 +23,13 @@ def check_repo(github, pagerduty, repo, warn_time)
   master_last = get_last(master)
   develop_last = get_last(develop)
 
-  return if master_last > develop_last
+  if master_last > develop_last
+    time_since_merge = ((develop_last - master_last) / 60).to_i
+    puts "#{repo} ok, was released after #{time_since_merge.abs} minutes"
+    incident = pagerduty.get_incident(develop['commit']['sha'])
+    incident.resolve unless incident.nil?
+    return
+  end
 
   # Time since last merged to develop
   time_since_merge = ((Time.now - develop_last) / 60).to_i
@@ -31,8 +37,6 @@ def check_repo(github, pagerduty, repo, warn_time)
   if time_since_merge > warn_time
     puts "#{repo} bad! code is #{time_since_merge} minutes old and not released."
     pagerduty.trigger("#{repo} has unreleased commits for #{time_since_merge} mintues.", incident_key: develop['commit']['sha'])
-  else
-    puts "#{repo} ok, was released after #{time_since_merge.abs} minutes"
   end
 end
 
